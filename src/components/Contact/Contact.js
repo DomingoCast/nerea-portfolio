12cb61623180f7b0fd25ba6b8eb6c836fe46f9c5
sortzgandea <castillomartidomingo@gmail.com>
2020-08-25 13:56:58 +0000

    contact and about

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,8 +1,15 @@
 import React from 'react'
 
+import classes from './Contact.module.sass'
+
+import info from '../../private/info'
+
 const contact = (props) => {
     return (
-        <p> contact me </p>
+        <div className={classes.container}>
+            <p className={classes.p}> T.{info.tlf} </p>
+            <p className={classes.p}> E.{info.email} </p>
+        </div>
     )
 }
 
