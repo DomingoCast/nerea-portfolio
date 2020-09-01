beeb141a28fb37411d1c2c1c422c7a33f05667cf
sortzgandea <castillomartidomingo@gmail.com>
2020-09-01 12:05:08 +0000

    changed nav and images

---
+++ src/assets/database/sessions.js
@@ -0,0 +1,59 @@
+
+import session1 from './session1/IMG_1190.jpg'
+import session2 from './session2/1aFoto.jpg'
+import session3 from './session3/01.jpg'
+import session4 from './session4/1ª.jpg'
+//import session5 from './nerea-about.png'
+
+//const sessions = {
+    //session1: {
+        //name: 'session1',
+        //preview: session1,
+        //path: 'session1',
+    //},
+    //session2: {
+        //name: 'session2',
+        //preview: session2,
+        //path: 'session2',
+    //},
+    //session3: {
+        //name: 'session3',
+        //preview: session3,
+        //path: 'session3',
+    //},
+    //session4: {
+        //name: 'session4',
+        //preview: session4,
+        //path: 'session4',
+    //},
+    //session5: {
+        //name: 'session5',
+        //preview: session5,
+        //path: 'session5'
+    //},
+//}
+
+const sessions = [
+    {
+        name: 'autoretrato',
+        preview: session1,
+        path: 'session1',
+    },
+    {
+        name: 'paths',
+        preview: session2,
+        path: 'session2',
+    },
+    {
+        name: 'cuarentena',
+        preview: session3,
+        path: 'session3',
+    },
+    {
+        name: 'retrato',
+        preview: session4,
+        path: 'session4',
+    },
+]
+
+export default sessions
