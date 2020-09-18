f56dd84b9a0cca53021d8792691a4a5946dc9671
Domingo Castillo <castillomartidomingo@gmail.com>
2020-09-18 09:44:56 +0200

    added title and tried 404 page

---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import classes from './NotFound.module.sass'
+
+import { Link  } from 'react-router-dom'
+
+const notFound = () => {
+
+    return (
+            <div className={classes.container}>
+        <Link to='/home'>
+                <h1 className={classes.h1}> 404 notFound </h1>
+                <p className={classes.p}> No hemos encontrado la página, haz click para volver al inicio </p>
+        </Link>
+            </div>
+    )
+}
+
+export default notFound
