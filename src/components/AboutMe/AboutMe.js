d514410fb2383aba6372810a6b2d0d5b61f8035d
Domingo Castillo <castillomartidomingo@gmail.com>
2020-09-26 11:39:44 +0200

    new text aboutme and added float query

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -10,9 +10,19 @@ const aboutMe = (props) => {
             <div className={classes.imgContainer}>
                 <img className={classes.img} src={Nerea}/>
             </div>
-            <p className={classes.p}> 
-                Soy una fotografa de elche. Formerly known as Nerelx. Tengo un perro, dos gatos y una paloma que me lo caga todo pero la quiero de todas formas. Soy super heavy y no me gustan las relaciones serias
-            </p>
+            <div className={classes.p}> 
+        <p className={classes.pr}>
+                Soy una fotógrafa de Alicante que se espeliaciza en fotografía de retrato aunque también tengo una leve obsesión por lo abstracto y el diseño.
+        </p>
+
+        <p className={classes.pr}>
+                Concretamente nací en Elche,Alicante donde curse un Grado Medio de “Artes Gráficas” fue mi primer paso al mundo de las Artes. Gracias a este curso trabajé como diseñadora gráfica en una importante empresa llamada BLINKER en la que solo trabajé durante tres meses, por motivos de estudio.
+        </p>
+
+        <p className={classes.pr}>
+                Actualmente estoy cursando un Grado Superior de “Iluminación” en San Juan, Alicante. Posteriormente trabajé durante 3 años en una agencia de modelos llamada “IMAGINE” en Elche, mi trabajo era hacer books a modelos de la empresa.
+        </p>
+            </div>
         </div>
     )
 }
