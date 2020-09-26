1b1f02035d52f3169d848e049e2efa6a188b0110
Domingo Castillo <castillomartidomingo@gmail.com>
2020-09-26 18:05:40 +0200

    fallo intento swipe popup

--- src/components/Popup/Popup.js
+++ src/components/Popup/Popup.js
@@ -1,13 +1,46 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 import classes from './Popup.module.sass'
 
-const popup = (props) => {
+const Popup = (props) => {
+    const [touchStart, setTS] = useState(false)
+    const [touchMove, setTM] = useState(false)
+    const [distance, setD] = useState(null)
+    const [init, setI] = useState(null)
+    const [current, setC] = useState(null)
+
+    const handleStart = (e) => {
+        if(!touchStart)
+            setI(e.touches[0].pageX)
+            setTS(true)
+    }
+    const handleMove = (e) => {
+            setC(e.touches[0].pageX)
+    }
+
+    const handleStop = (e) => {
+        if(current - init > 100 && touchStart){
+            props.next(e)
+        }
+        if(current - init < 100 && touchStart){
+            props.previous(e)
+        }
+        setTS(false)
+        setI(null)
+
+    }
+
+    //if(props.display){
+        //document.addEventListener('touchstart', (e) => handleStart(e))
+        //document.addEventListener('touchmove', (e) => handleMove(e))
+        //document.addEventListener('touchend', (e) => handleStop(e))
+    //}
+
     return(
         <div  className={classes.backdrop+' '+(props.display?null:classes.hidden)}>
             <a href="#" className={classes.backLink} onClick={props.popDown}/>
             <div className={classes.imgContainer}>
-                <img className={classes.img} src={props.image} alt="expanded superpwer"/>
+                <img onTouchStart={() => console.log('holaj')} className={classes.img} src={props.image} alt="expanded superpwer"/>
                 <a href="#" className={classes.link +' '+classes.left} onClick={props.previous}/>
                 <a href="#" className={classes.link +' '+classes.right} onClick={props.next}/>
                 <a href="#" className={classes.back} onClick={props.popDown}>back</a>
@@ -16,4 +49,4 @@ const popup = (props) => {
     )
 }
 
-export default popup
+export default Popup
