a65e7c8e388770f4a8a3c5ec9a7b94e3eb477ec3
Domingo Castillo <castillomartidomingo@gmail.com>
2020-09-27 13:21:11 +0200

    put back button inside

--- src/components/Title/Title.js
+++ src/components/Title/Title.js
@@ -7,7 +7,7 @@ import { Link, NavLink } from 'react-router-dom'
 const title = (props) => {
     return(
         <div className={classes.container}>
-                <Link to="/home" className={classes.link}>
+            <Link to="/home" className={classes.link}>
             <h1 className={classes.h1}>
                     Nerea Molina
             </h1>
